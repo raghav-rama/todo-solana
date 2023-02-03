b8406ad1a66e46fa838f1193afee1f3ec61d308b
raghav-rama <hackerer528@gmail.com>
2023-02-03 22:43:39 +0530

    feat: update new deployed id

--- programs/todo-solana/src/lib.rs
+++ programs/todo-solana/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("5MwohuA1kjtkXfugMqchkyhnUjWZQf9GGWgnHTyW1977");
 
 #[program]
 pub mod todo_solana {
