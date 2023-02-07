cb79d3a38eac8d4707cc35f56c1a275b2188cd22
raghav-rama <hackerer528@gmail.com>
2023-02-07 05:59:22 +0530

    draft: update program

--- programs/todo-solana/src/lib.rs
+++ programs/todo-solana/src/lib.rs
@@ -6,41 +6,71 @@ declare_id!("5MwohuA1kjtkXfugMqchkyhnUjWZQf9GGWgnHTyW1977");
 pub mod todo_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Initialize!");
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.todo = String::from("What do u wanna do?");
-        my_account.done = false;
-        my_account.time = Clock::get().unwrap().unix_timestamp;
-        msg!("Todo: {}", my_account.todo);
-        msg!("Done?: {}", my_account.done);
-        msg!("Time created: {}", my_account.time);
+    pub fn initialize(ctx: Context<InitializeTodo>) -> Result<()> {
+        let pubkey = ctx.accounts.todo_item.to_account_info().key();
+
+        let head = &mut ctx.accounts.genesis_todo_account;
+        let current = &mut ctx.accounts.current;
+        let todo_item = &mut ctx.accounts.todo_item;
+
+        todo_item.todo = String::from("Genesis Todo");
+        todo_item.done = false;
+        todo_item.time = Clock::get()?.unix_timestamp;
+        todo_item.next = Pubkey::default();
+        
+        current.current = pubkey;
+        head.head = pubkey; 
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = MyAccount::MAX_SIZE)]
-    pub my_account: Account<'info, MyAccount>,
+pub struct InitializeTodo<'info> {
+    #[account(init, payer = user, space = TodoItem::MAX_SIZE)]
+    pub todo_item: Account<'info, TodoItem>,
+
+    #[account(init, payer = user, space = CurrentTodoItem::MAX_SIZE)]
+    pub current: Account<'info, CurrentTodoItem>,
+
+    #[account(init, payer = user, space = Head::MAX_SIZE)]
+    pub genesis_todo_account: Account<'info, Head>,
+
     #[account(mut)]
     pub user: Signer<'info>,
+
     pub system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(mut)]
-    pub my_account: Account<'info, MyAccount>
-}
-
+// #[derive(AnchorDeserialize, AnchorSerialize, Clone)]
 #[account]
-pub struct MyAccount {
+pub struct TodoItem {
     pub todo: String,
     pub done: bool,
     pub time: i64,
+    pub next: Pubkey,
 }
 
-impl MyAccount {
-    pub const MAX_SIZE: usize = 4 + 19 + 1 + 8;
+#[account]
+pub struct Head {
+    pub head: Pubkey,
 }
+
+#[account]
+pub struct CurrentTodoItem {
+    pub current: Pubkey,
+}
+
+impl TodoItem {
+    pub const MAX_SIZE: usize = 8 + 4 + 200 + 1 + 8 + 8; // Todo String length is 200
+}
+
+impl Head {
+    pub const MAX_SIZE: usize = 8 + 32;
+}
+
+impl CurrentTodoItem {
+    pub const MAX_SIZE: usize = 8 + 32;
+}
+
+
